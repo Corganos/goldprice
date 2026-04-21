6d89944e7502bf6361977972d206947607f9fcdd
Corgano <122217939+Corganos@users.noreply.github.com>
2026-04-21 18:25:27 -0400

    delete debug i

--- public/api/debug.php
+++
@@ -1,7 +0,0 @@
-<?php
-$CONFIG = require '/home8/ivancicg/private/config.php';
-$key = urlencode($CONFIG['metalprice_key']);
-$url = "https://api.metalpriceapi.com/v1/latest?api_key={$key}&base=USD&currencies=XAU,XAG,XPT,XPD,EUR,GBP,JPY";
-$body = file_get_contents($url);
-header('Content-Type: application/json');
-echo $body;
